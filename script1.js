458a9425189f73815de756622817bdfd86512724
frnslt <105853299+frnslt@users.noreply.github.com>
2025-01-05 12:35:58 +0100

    Update script1.js
    Updated database.js file path for import

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
-import { database } from './database.js'; // Importa il database dal file separato
+import { database } from 'database.js'; // Importa il database dal file separato
 
 // Variabili per la password
-const PASSWORD = "securepassword123"; // Sostituisci con la tua password
+const PASSWORD = "securepassword123";
 let isAuthenticated = false;
 
 // Elementi della UI
